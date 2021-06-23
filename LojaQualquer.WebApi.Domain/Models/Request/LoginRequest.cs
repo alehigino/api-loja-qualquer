725289a1623265dae86a7f1b282c6dc9caa6a98a
DESKTOP-TAV9FGC\alexandre <alehigino@yahoo.com.br>
2021-06-23 00:36:20 -0300

    Inserindo validacoes no modelo de login

--- LojaQualquer.WebApi.Domain/Models/Request/LoginRequest.cs
+++ LojaQualquer.WebApi.Domain/Models/Request/LoginRequest.cs
@@ -1,8 +1,15 @@
-﻿namespace LojaQualquer.WebApi.Domain.Models.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LojaQualquer.WebApi.Domain.Models.Request
 {
     public class LoginRequest
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "É obrigatório informar o email.")]
+        [MaxLength(128, ErrorMessage = "O email deve conter até 128 caracteres.")]
         public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "É obrigatório informar a senha.")]
+        [MaxLength(30, ErrorMessage = "A senha deve conter até 30 caracteres.")]
         public string Password { get; set; }
     }
 }
